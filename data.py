a5071b1ad344f6ef023792bc8de256eacffea977
Miroslav Kucera <vvendigo@seznam.cz>
2010-04-27 16:56:04 +0200

    tile generation rutine enhanced

--- data.py
+++ data.py
@@ -27,15 +27,38 @@ def loadTiles(fName, tileSize):
     return out
 #enddef
 
-def makeTile(setup = [[1,1,1],[1,1,1],[1,1,1]]):
+def makeTile(setup = [[1,1,1],[1,0,1],[1,1,1]]):
     """ Make tile filled with random noise
     """
+    smpSize = core.tileSize/4 # sample radius
+    smpArea = float((2*smpSize)*(2*smpSize))
 
     s = pygame.Surface((core.tileSize, core.tileSize))
     for x in xrange(0, core.tileSize):
         for y in xrange(0, core.tileSize):
+            px = smpSize
+            py = smpSize
+            sx1 = x - smpSize
+            sy1 = y - smpSize
+            sx2 = x + smpSize
+            sy2 = y + smpSize
+            if sx1 < 0: px = -sx1
+            if sy1 < 0: py = -sy1
+            if sx2 >= core.tileSize: px = 2*smpSize - (sx2-core.tileSize)
+            if sy2 >= core.tileSize: py = 2*smpSize - (sy2-core.tileSize)
+            opacity = 0.0
+            if px>=0 and py>=0:
+                opacity = (\
+                      setup[sy1/core.tileSize+1][sx1/core.tileSize+1]*(px*py)\
+                    + setup[sy1/core.tileSize+1][sx2/core.tileSize+1]*((2*smpSize-px)*py)\
+                    + setup[sy2/core.tileSize+1][sx2/core.tileSize+1]*((2*smpSize-px)*(2*smpSize-py))\
+                    + setup[sy2/core.tileSize+1][sx1/core.tileSize+1]*(px*(2*smpSize-py)) ) / smpArea
+            #print opacity
             r = randint(20,150)
-            s.set_at((x,y), (r+90, r+randint(-r/10,r/10), r+randint(-r/20,+r/20)))
+            s.set_at((x,y),\
+                (int((r+90)*opacity),\
+                int((r+randint(-r/10,r/10))*opacity),\
+                int((r+randint(-r/20,+r/20))*opacity)))
     #endfor
 
     return s.convert()
@@ -49,8 +72,10 @@ def init():
     alien = load('img/alien.png')
     missile = load('img/missile.png')
     tiles = []
-    for i in xrange(0,6):
+    for i in xrange(0,1):
         tiles.append(makeTile())
+#    import sys
+#    sys.exit()
     #loadTiles('img/tiles.png', core.tileSize)
     mnuFont1 = pygame.font.Font(pygame.font.get_default_font(), 30)
     mnuFont2 = pygame.font.Font(pygame.font.get_default_font(), 50)
